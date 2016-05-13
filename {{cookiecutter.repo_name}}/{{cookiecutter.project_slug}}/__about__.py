4f00e3cd57654677ae588d780ac2f78798b3659c
Vince Veselosky <vince@control-escape.com>
2016-05-13 11:21:06 -0400

    some less gross

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_slug}}/__about__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_slug}}/__about__.py
@@ -16,6 +16,6 @@ __copyright__ = "{{ cookiecutter.year }} Vince Veselosky and contributors"
 __description__ = "{{ cookiecutter.project_short_description }}"
 __license__ = "Apache 2.0"
 __name__ = "{{ cookiecutter.project_name }}"
-__url__ = 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}'
+__url__ = 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}'
 __version__ = "0.1.0"
 
